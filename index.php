93ddb870b71a8adc368b16071bebecc1e0def657
Ansab Naif <ansab20556@gmail.com>
2025-08-13 00:18:44 +0300

    Add POST form with name and favorite color fields to index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    
+    $currentTime = date('H:i:s');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Server Time & Form</title>
+</head>
+<body>
+    <h1>Current Server Time: <?php echo $currentTime; ?></h1>
+
+    <form action="handle_form.php" method="POST">
+        <label for="name">Your Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="color">Favorite Color:</label>
+        <input type="color" id="color" name="color" required>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
